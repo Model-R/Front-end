c0645515b94880dd4195496e73232e5e41650cc4
antunesmg <antunesmg93@gmail.com>
2019-03-13 08:45:12 -0300

    backup js 13/03

--- js/loading.js
+++ js/loading.js
@@ -1,5 +1,9 @@
 
 function exibe(id, text) { 
+	console.log(text)
+	console.log('dwqdwq')
+	console.log(document.getElementById('loading-title'))
+	console.log(document.getElementById('loading-title').innerHTML)
     if(text != '') document.getElementById('loading-title').innerHTML = text;
 	if(document.getElementById(id).style.display=="none") {  
 	document.getElementById(id).style.display = "inline";  
