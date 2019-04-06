03c612da7ee1fc743051ff0e3bc81334911ae005
Marcos Antunes <antunesmg93@gmail.com>
2019-04-06 10:31:50 -0300

    removing v2 folder

--- v2/templates/ordenarfiltro.php
+++
@@ -1,8 +0,0 @@
-<div class="form-group">
-    <label for="cmboxordenar">Ordenar por</label>
-    <select id="cmboxordenar" name="cmboxordenar" class="form-control">
-        <option value="USUARIO" <?php if ($ordenapor=='USUARIO') echo "selected";?>>Usuário</option>
-        <option value="EXPERIMENTO" <?php if ($ordenapor=='EXPERIMENTO') echo "selected";?>>Experimento</option>
-        <option value="GRUPO" <?php if ($tipofiltro=='GRUPO') echo "selected";?>>Grupo</option>
-    </select>
-</div>
